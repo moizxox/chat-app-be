bd3f3774d0678b90ecafdeb4c4aa27da5e7956e8
Mian Muhammad Moiz <164487066+moizxox@users.noreply.github.com>
2025-11-17 15:32:08 +0500

    user modal and db added

--- user/src/index.ts
+++ user/src/index.ts
@@ -1,7 +1,16 @@
 import express from "express";
+import dotenv from "dotenv";
+import connectDB from "./config/db.js";
+
+dotenv.config();
+
+connectDB();
 
 const app = express();
+const port = process.env.PORT;
 
-const port = 5000;
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
 
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => console.log(`Server running on port http://localhost:${port}`));
