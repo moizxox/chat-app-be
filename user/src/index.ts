43a13880ef71cb67895f88e9a4bbb28afe4c9d54
Mian Muhammad Moiz <164487066+moizxox@users.noreply.github.com>
2025-11-17 18:20:44 +0500

    user controller and route file added

--- user/src/index.ts
+++ user/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/db.js";
 import { createClient } from "redis";
+import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
@@ -18,6 +19,8 @@ redisClient
 
 const app = express();
 const port = process.env.PORT;
+
+app.use("/api/v1", userRoutes);
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
