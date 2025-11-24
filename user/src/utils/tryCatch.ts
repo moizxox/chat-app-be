1db60ebf84bc4461dd9dfe7450251268339e0729
Mian Muhammad Moiz <164487066+moizxox@users.noreply.github.com>
2025-11-24 11:26:37 +0500

    utils added

--- user/src/utils/tryCatch.ts
+++ user/src/utils/tryCatch.ts
@@ -1,16 +1,16 @@
 import type { NextFunction, Request, Response, RequestHandler } from "express";
 
-const asyncHandler =
+const tryCatch =
   (handler: RequestHandler): RequestHandler =>
   async (req: Request, res: Response, next: NextFunction) => {
     try {
       await handler(req, res, next);
     } catch (error) {
       res.status(500).json({
-        message: "Something went wrong in asyncHandler",
+        message: "Something went wrong in tryCatch",
         error,
       });
     }
   };
 
-export { asyncHandler };
+export { tryCatch };
