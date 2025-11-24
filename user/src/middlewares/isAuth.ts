00deb208675e738ebed6c1057fe8590cdbd89822
Mian Muhammad Moiz <164487066+moizxox@users.noreply.github.com>
2025-11-24 17:52:08 +0500

    is auth middlware setuped

--- user/src/middlewares/isAuth.ts
+++ user/src/middlewares/isAuth.ts
@@ -1,12 +1,30 @@
-import type { NextFunction, Request } from "express";
+import type { Request, Response, NextFunction } from "express";
 import type { IUser } from "../model/User.model.js";
+import { CustomError } from "../utils/customError.js";
+import jwt, { type JwtPayload } from "jsonwebtoken";
+import User from "../model/User.model.js";
 
 export interface AuthenticatedReq extends Request {
   user?: IUser | null;
 }
 
-export const isAuth = (req:AuthenticatedReq, res:Response, next:NextFunction)=>{
-    Promise<void>=>{
-        
+export const isAuth = async (req: AuthenticatedReq, res: Response, next: NextFunction) => {
+  try {
+    const authHeader = req.headers.authorization;
+
+    if (!authHeader || !authHeader.startsWith("Bearer ")) return next(new CustomError(401, "Unauthorized"));
+
+    const token = authHeader.split(" ")[1];
+
+    const decoded = jwt.verify(token as string, process.env.JWT_SECRET!) as JwtPayload;
+
+    if (!decoded || !decoded.user) {
+      return next(new CustomError(401, "Unauthorized"));
     }
-}
+
+    req.user = decoded.user;
+    next();
+  } catch (error) {
+    return next(new CustomError(500, "Internal Server Error"));
+  }
+};
