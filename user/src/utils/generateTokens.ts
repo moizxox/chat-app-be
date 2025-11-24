ad65609a8d6fc4df28748cb4b57ff606ee098d6a
Mian Muhammad Moiz <164487066+moizxox@users.noreply.github.com>
2025-11-24 12:20:08 +0500

    tokens util added

---
+++ user/src/utils/generateTokens.ts
@@ -0,0 +1,13 @@
+import jwt from "jsonwebtoken";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const JWT_SECRET = process.env.JWT_SECRET as string;
+
+export const generateTokens = (user: any) => {
+  const accessToken = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "1d" });
+  const refreshToken = jwt.sign({ id: user._id }, JWT_SECRET, { expiresIn: "7d" });
+
+  return { accessToken, refreshToken };
+};
