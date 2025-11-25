3791371c6d2a4405ce236c02d471c7b52b556bba
Mian Muhammad Moiz <164487066+moizxox@users.noreply.github.com>
2025-11-25 11:46:27 +0500

    user route auth fix for some apis

--- user/src/routes/user.route.ts
+++ user/src/routes/user.route.ts
@@ -8,7 +8,7 @@ router.post("/login", loginUser);
 router.post("/verify", verifyUser);
 router.get("/me", isAuth, myProfile);
 router.put("/update-name", isAuth, updateName);
-router.get("/all", isAuth, getAllUsers);
-router.get("/:id", isAuth, getAUser);
+router.get("/all", getAllUsers);
+router.get("/:id", getAUser);
 
 export default router;
