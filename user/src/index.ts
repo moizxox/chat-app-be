3721b64dcff5f48933410c5ff78c5a095dae7848
Mian Muhammad Moiz <164487066+moizxox@users.noreply.github.com>
2025-11-25 11:44:13 +0500

    cors added

--- user/src/index.ts
+++ user/src/index.ts
@@ -4,6 +4,7 @@ import connectDB from "./config/db.js";
 import { createClient } from "redis";
 import userRoutes from "./routes/user.route.js";
 import { connectRabbitMQ } from "./config/rabbitmq.js";
+import cors from "cors";
 
 dotenv.config();
 
@@ -20,6 +21,8 @@ redisClient
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
 const port = process.env.PORT;
 
 app.use("/api/v1", userRoutes);
